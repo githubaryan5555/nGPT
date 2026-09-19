756b1ae202aa5ab7ac0300065f8e37f473a10822
githubaryan5555 <144113177+githubaryan5555@users.noreply.github.com>
2026-09-20 05:02:09 +0530

    Increase batch size from 4 to 64

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ wandb_run_name = 'fw' # 'run' + str(time.time())
 # data
 dataset = 'fineweb'
 gradient_accumulation_steps = 8 * 8 # used to simulate larger batch sizes
-batch_size = 4 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 64 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 256
 # model
 n_layer = 4
