07f37573f201ede79d2eccdf934372d2b0744cd8
githubaryan5555 <144113177+githubaryan5555@users.noreply.github.com>
2026-09-20 05:15:56 +0530

    Reduce gradient accumulation steps from 64 to 32

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ wandb_project = 'owt'
 wandb_run_name = 'fw' # 'run' + str(time.time())
 # data
 dataset = 'fineweb'
-gradient_accumulation_steps = 8 * 8 # used to simulate larger batch sizes
+gradient_accumulation_steps = 8 * 4 # used to simulate larger batch sizes
 batch_size = 64 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 256
 # model
