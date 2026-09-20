0422a5c7feb33018e57d489d48cc772f56b0ff25
githubaryan5555 <144113177+githubaryan5555@users.noreply.github.com>
2026-09-21 05:22:58 +0530

    Adjust batch size and gradient accumulation steps

--- test1/config.py
+++ test1/config.py
@@ -30,8 +30,8 @@ dropout = 0.0
 bias = False
 
 # ---------------------------- batching -------------------------
-batch_size = 512
-gradient_accumulation_steps = 32
+batch_size = 256
+gradient_accumulation_steps = 64
 
 # --------------------------- optimizer -------------------------
 learning_rate = 1e-3
