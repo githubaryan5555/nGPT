f4cd20d95d2cfadba642f8c078fb635a6e6a74c3
githubaryan5555 <144113177+githubaryan5555@users.noreply.github.com>
2026-09-20 05:33:25 +0530

    Update training parameters for model configuration

--- train.py
+++ train.py
@@ -33,9 +33,9 @@ from model import GPTConfig, GPT
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out_fineweb'
-eval_interval = 5
-log_interval = 1
-eval_iters = 5
+eval_interval = 100
+log_interval = 10
+eval_iters = 25
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
@@ -49,22 +49,22 @@ gradient_accumulation_steps = 4 * 4 # used to simulate larger batch sizes
 batch_size = 64 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 256
 # model
-n_layer = 6
+n_layer = 8
 n_head = 8
 n_embd = 256
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
 learning_rate = 1e-3 # max learning rate
-max_iters = 500 # total number of training iterations
+max_iters = 1000 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 5 # how many steps to warm up for
-lr_decay_iters = 500 # should be ~= max_iters per Chinchilla
+warmup_iters = 50 # how many steps to warm up for
+lr_decay_iters = 1000 # should be ~= max_iters per Chinchilla
 min_lr = 5e-4 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
 backend = 'nccl' # 'nccl', 'gloo', etc.
