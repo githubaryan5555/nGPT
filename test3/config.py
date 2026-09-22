f6381bc8f5f776d34688db380502ca0a323bbe79
githubaryan5555 <144113177+githubaryan5555@users.noreply.github.com>
2026-09-22 18:17:25 +0530

    Increase model parameters for hidden layers and attention heads

--- test3/config.py
+++ test3/config.py
@@ -5,13 +5,13 @@
 # Vocabulary / Model
 vocab_size = 8192
 hidden_size = 256
-num_hidden_layers = 8
+num_hidden_layers = 16
 
 # MLP / SwiGLU
 intermediate_size = 1024
 
 # Attention / GQA
-num_attention_heads = 8
+num_attention_heads = 16
 num_key_value_heads = 4
 attention_dropout = 0.0
 
